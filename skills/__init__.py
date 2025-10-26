391a9dde85435ad2043b4a9f102c45fb1636618a
nani5911 <monikanenna0@gmail.com>
2025-10-26 15:48:54 +0530

    Update __init__.py

--- skills/__init__.py
+++ skills/__init__.py
@@ -7,6 +7,6 @@ package_dir = os.path.dirname(__file__)
 # Discover all modules in the skills directory
 __all__ = [
     name
-    for _, name, _ in pkgutil.iter_modules([package_dir])
+    for pkgutil.iter_modules([package_dir])
     if not name.startswith("_") and not name == "base"
 ]
